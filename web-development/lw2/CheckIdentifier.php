3d313e3af3db6f340a0fb73aa39a2b72a3445dc8
Dudin Alexey <dudin.lesha@gmail.com>
2022-02-22 23:18:15 +0300

    Fix gitignore

--- web-development/lw2/CheckIdentifier.php
+++ web-development/lw2/CheckIdentifier.php
@@ -1,9 +1,22 @@
 <?php
 $request = $_GET['identifier'];
+echo $request;
 echo checkOnParam($request);
 
 
 function checkOnParam($val)
 {
-
+    for ($i = 0; $i < strlen($val); $i++)
+    {
+        if ($i == 0)
+        {
+            if (!ctype_alpha($val[$i]))
+                return 'false';
+        }
+        if (!ctype_alpha($val[$i]) && !is_numeric($val[$i]))
+        {
+            return 'false';
+        }
+    }
+    return 'true';
 }
